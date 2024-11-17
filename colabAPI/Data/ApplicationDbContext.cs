e36b18074e9615b76808d5b843b0d199dcafba03
alexrossoni <alexjuniorjn@hotmail.com>
2024-11-17 11:03:33 -0300

    feat: implementação inicial de controle de projetos

--- colabAPI/Data/ApplicationDbContext.cs
+++ colabAPI/Data/ApplicationDbContext.cs
@@ -12,5 +12,25 @@ namespace colabAPI.Data
 
         // Defina aqui as tabelas como DbSet
         public DbSet<Financiador> Financiadores { get; set; }
+        public DbSet<Projeto> Projetos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Projeto>()
+                .HasOne(p => p.Financiador)
+                .WithMany(f => f.Projetos)
+                .HasForeignKey(p => p.FinanciadorId)
+                .OnDelete(DeleteBehavior.Restrict); // não permite exclusão caso exista relacionamento
+
+            modelBuilder.Entity<Projeto>()
+                .Property(p => p.Categoria)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Projeto>()
+                .Property(p => p.Status)
+                .HasConversion<string>();
+        }
     }
 }
