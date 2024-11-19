4ad9c42a4e12ff81287eb84657e54cf8276aef06
Sofia de Alcantara <sofiaalcantaras@outlook.com>
2024-11-19 02:27:33 -0300

    feat: implementação de Financiador finalizada

--- colabAPI/Data/ApplicationDbContext.cs
+++ colabAPI/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace colabAPI.Data
 {
+    // Classe que representa o contexto do banco de dados, estendendo DbContext do Entity Framework
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
@@ -10,19 +11,21 @@ namespace colabAPI.Data
         {
         }
 
-        // Defina aqui as tabelas como DbSet
+        // Definição das tabelas (DbSet) do banco de dados que serão mapeadas pelo EF
         public DbSet<Financiador> Financiadores { get; set; }
         public DbSet<Projeto> Projetos { get; set; }
 
+        // Configurações adicionais para o mapeamento do modelo relacional
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // Relacionamento entre 'Projeto' e 'Financiador'
             modelBuilder.Entity<Projeto>()
                 .HasOne(p => p.Financiador)
                 .WithMany(f => f.Projetos)
                 .HasForeignKey(p => p.FinanciadorId)
-                .OnDelete(DeleteBehavior.Restrict); // não permite exclusão caso exista relacionamento
+                .OnDelete(DeleteBehavior.Restrict); // Não permite exclusão caso exista relacionamento
 
             modelBuilder.Entity<Projeto>()
                 .Property(p => p.Categoria)
