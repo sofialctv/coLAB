81576c9cba45f1b94a6708bb15efb517149c93c9
Sofia de Alcantara <sofiaalcantaras@outlook.com>
2024-11-22 02:20:14 -0300

    feat: primeira tentativa de 'esconder' string de conexão no Docker

--- colabAPI/Program.cs
+++ colabAPI/Program.cs
@@ -2,11 +2,16 @@
 using colabAPI.Business.Repository.Interfaces;
 using colabAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // String de conexão com o banco de dados
+//var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+//                       ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Carregar a string de conexão da variável de ambiente (substitui o appsettings.json)
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Configuração do PostgreSQL
@@ -17,7 +22,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers().AddJsonOptions(options =>
    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
-
+// Injeção de dependências
 builder.Services.AddScoped<IBolsaRepository, BolsaRepository>();
 builder.Services.AddScoped<IBolsistaRepository, BolsistaRepository>();
 builder.Services.AddScoped<IFinanciadorRepository, FinanciadorRepository>();
@@ -29,8 +34,24 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
 
-
 var app = builder.Build();
+
+//// Rodar as migrações no startup da aplicação
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+//    try
+//    {
+//        // Pega o contexto do banco de dados (ApplicationDbContext) e aplica as migrações
+//        var context = services.GetRequiredService<ApplicationDbContext>();
+//        context.Database.Migrate();  // Aplica as migrações pendentes ao banco de dados
+//    }
+//    catch (Exception ex)
+//    {
+//        var logger = services.GetRequiredService<ILogger<Program>>();
+//        logger.LogError(ex, "ERRO: Intercorrência ao aplicar migrações.");
+//    }
+//}
 
 if (app.Environment.IsDevelopment())
 {
