eb9c3c94d90a892267bf00ff63847c7364e1aa1b
Ana Julia <anajcaetanom@gmail.com>
2025-01-30 16:54:17 -0300

    .

---
+++ coLAB/AutoMapper/ConfigMapping.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using colab.Business.DTOs.Request;
+using colab.Business.DTOs.Response;
+using colab.Business.Models.Entities;
+
+namespace colab.AutoMapper;
+
+public class ConfigMapping : Profile
+{
+    public ConfigMapping()
+    {
+        CreateMap<Pessoa, PessoaResponseDTO>()
+            .ReverseMap();
+        CreateMap<Pessoa, PessoaRequestDTO>()
+            .ReverseMap();
+        
+        CreateMap<Cargo, CargoResponseDTO>()
+            .ReverseMap();
+        CreateMap<Cargo, CargoRequestDTO>()
+            .ReverseMap();
+        
+        CreateMap<HistoricoCargo, HistoricoCargoResponseDTO>()
+            .ForMember(dest => dest.CargoNome, opt 
+                => opt.MapFrom(src => src.Cargo.Nome))
+            .ForMember(dest => dest.PessoaNome, opt 
+                => opt.MapFrom(src => src.Pessoa.Nome))
+            .ReverseMap();
+        CreateMap<HistoricoCargo, HistoricoCargoRequestDTO>();
+        CreateMap<HistoricoCargoRequestDTO, HistoricoCargo>()
+            .ForMember(dest => dest.Cargo, opt 
+                => opt.Ignore());
+    }
+}
